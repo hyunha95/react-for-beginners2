5a65922244293c8d2fe2fc7ee927b2b8fcc86626
hyunha95 <nohyunha95@gmail.com>
2022-06-08 11:08:48 +0900

    fetch사용해서 api값 불러오기

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import { useState, useEffect } from "react";
 
-function Hello(){
-  function byeFn() {
-    console.log("bye :(");
-  }
-  function hiFn() {
-    console.log("created :)");
-    return byeFn;
-  }
-  useEffect(hiFn, []);
-  return <h1>Hello</h1>
-}
-
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
-  return( 
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [money, setMoney] = useState(0);
+  const [value, setValue] = useState(0);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json);
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+  const onChange = (event) => {
+    setMoney(event.target.value);
+  }
+  const calculate = (event) => {
+    console.log(event);
+    setValue(event.target.value);
+  }
+  return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      <div>
+        <label htmlFor="money">how much you want</label>
+        <input type="number" id="money" value={money} placeholder="money" onChange={onChange}/>
+      </div>
+      <div>
+        <label htmlFor="change">value</label>
+        <input type="number" value={Number(money / value)} placeholder="value"/>
+      </div>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <select onChange={calculate} value={value}>
+          {coins.map((coin) => (
+            <option value={coin.quotes.USD.price} >
+              {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
+            </option>
+          ))}
+        </select>
+      )}
     </div>
-  )
+  );
 }
 
 export default App;
