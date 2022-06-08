ada51dcb93ba5ed023f08e74c145b99a59b3fd80
hyunha95 <nohyunha95@gmail.com>
2022-06-08 13:40:22 +0900

    컴포넌트 임포트하여 사용

--- src/App.js
+++ src/App.js
@@ -1,81 +1,40 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
+import Movie from "./components/Movie";
 
 function App() {
-<<<<<<< HEAD
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [money, setMoney] = useState(0);
-  const [value, setValue] = useState(0);
-  useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        console.log(json);
-        setCoins(json);
-        setLoading(false);
-      });
-  }, []);
-  const onChange = (event) => {
-    setMoney(event.target.value);
-  }
-  const calculate = (event) => {
-    console.log(event);
-    setValue(event.target.value);
-  }
-  return (
-    <div>
-      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
-      <div>
-        <label htmlFor="money">how much you want</label>
-        <input type="number" id="money" value={money} placeholder="money" onChange={onChange}/>
-      </div>
-      <div>
-        <label htmlFor="change">value</label>
-        <input type="number" value={Number(money / value)} placeholder="value"/>
-      </div>
-      {loading ? (
-        <strong>Loading...</strong>
-      ) : (
-        <select onChange={calculate} value={value}>
-          {coins.map((coin) => (
-            <option value={coin.quotes.USD.price} >
-              {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
-            </option>
-          ))}
-        </select>
-      )}
-=======
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setToDos((currentArray) => [toDo, ...currentArray]);
-    setToDo("");
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
+      )
+    ).json();
+
+    setMovies(json.data.movies);
+    setLoading(false);
   };
-  console.log(toDos);
+  useEffect(() => {
+    getMovies();
+  }, []);
+
   return (
     <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="Write your to do..."
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
->>>>>>> a5e427909500ff737f17b261d89ce83352edb63d
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          {movies.map((movie) => (
+            <Movie
+              key={movie.id}
+              coverImg={movie.medium_cover_image}
+              title={movie.title}
+              summary={movie.summary}
+              genres={movie.genres}
+            />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
